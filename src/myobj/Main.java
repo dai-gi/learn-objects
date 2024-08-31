fc4458a94cebe453cd1902218fabcd499e3c946b
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-08-31 16:35:47 +0900

    車両クラスを作成

--- src/myobj/Main.java
+++ src/myobj/Main.java
@@ -3,14 +3,28 @@ package myobj;
 public class Main {
 	public static void main(String[] args) {
 		// インスタンス化
-		
+		Vehicle car = new Vehicle();
+		Vehicle bike = new Vehicle();
+		Vehicle track = new Vehicle();
 
 		// 所有
+		car.ownerShip("daisuke", "普通自動車");
+		bike.ownerShip("daisuke", "普通二輪車");
+		track.ownerShip("daisuke", "中型トラック");
 		
-		// 前進する
+		// 始動
+		car.starting();
+		bike.starting();
+		track.starting();
 		
 		// 停止する
+		car.stop();
+		bike.stop();
+		track.stop();
 		
 		// 給油する
+		car.refuel("少し");
+		bike.refuel("半分");
+		track.refuel("満タン");
 	}
 }
