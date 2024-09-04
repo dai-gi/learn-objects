82da539f78c65d7076f6edb3a94c4836c3389e16
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-09-05 00:50:40 +0900

    条件分岐の評価結果の内容を変更

--- src/obj1/Main.java
+++ src/obj1/Main.java
@@ -6,16 +6,18 @@ public class Main {
 		User user1 = new User();
 		User user2 = new User();
 		User user3 = new User();
-		new User();
 		
+		// 子供たちの属性を定義する
 		user1.input("たーくん", 5200);
 		user2.input("まこたん", 0);
-		user3.input("超富裕層Aさん", 2500000);
+		user3.input("こうくん", 2500000);
 		
+		// 子供のお金の使い方についてタグづけをする
 		user1.ranking();
 		user2.ranking();
 		user3.ranking();
 		
+		// 自分の子供について振り返る
 		user1.output();
 		user2.output();
 		user3.output();
--- src/obj1/User.java
+++ src/obj1/User.java
@@ -1,28 +1,43 @@
 package obj1;
 
 public class User {
+	/**
+	 * userName  名前
+	 * amount    金額
+	 * ranking   ランキング
+	 */
 	String userName;
 	int amount;
 	String ranking;
 	
-	int input(String inputName, int inputAmount) {
-		userName = inputName;
+	/**
+	 * 子供達の属性を定義する
+	 * @param inputName    子供の名前
+	 * @param inputAmount  お小遣い
+	 */
+	void input(String inputName, int inputAmount) {
+		this.userName = inputName;
 		this.amount = inputAmount;
-		return 15;
 	}
 	
+	/**
+	 * 子供達のお金の使い方についてタグづけをする
+	 */
 	void ranking() {
 		if (amount >= 1000000) {
-			ranking = "お得意様";
-		} else if (amount >= 1000000) {
-			ranking = "新規顧客";
-		} else if (amount >= 1000000) {
-			ranking = "新規顧客";
+			ranking = "お金の使い方が上手な子";
+		} else if (amount >= 100000) {
+			ranking = "お金の使い方が普通の子";
+		} else if (amount >= 1) {
+			ranking = "お金の使い方が苦手な子";
 		} else {
-			ranking = "冷やかし客";
+			ranking = "お金の使い方が荒い子";
 		}
 	}
 	
+	/**
+	 * 自分の子供について振り返る
+	 */
 	void output() {
 		System.out.println("名前:" + userName);
 		System.out.println("金額:" + amount);
