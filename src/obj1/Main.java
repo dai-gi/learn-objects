0176e7420a956bc3cdb0f1beb11116e86313d8ba
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-09-22 14:53:09 +0900

    ランキングメソッドから成績表メソッドに変更

--- src/obj1/Main.java
+++ src/obj1/Main.java
@@ -7,15 +7,15 @@ public class Main {
 		User user2 = new User();
 		User user3 = new User();
 		
-		// 子供たちに名前をお小遣いを授ける
+		// 子供たちに名前とお小遣いを授ける
 		user1.input("たーくん", 15000);
 		user2.input("まこたん", 0);
-		user3.input("こうくん", 2500000);
+		user3.input("こうくん", 250000);
 		
-		// 子供のお金の使い方に対して子供にランキングを発表する
-		user1.ranking();
-		user2.ranking();
-		user3.ranking();
+        // 子供から成績表を受け取るだけ
+		String receiveGradeBook1 = user1.gradebook();
+		String receiveGradeBook2 = user2.gradebook();
+		String receiveGradeBook3 = user3.gradebook();
 		
 		// 子供に対して自分の状態を教えるようにお願いする
 		user1.output();
--- src/obj1/User.java
+++ src/obj1/User.java
@@ -1,18 +1,18 @@
 package obj1;
+import java.util.Random;
 
 public class User {
-	/**
-	 * userName  名前
-	 * amount    金額
-	 * ranking   ランキング
-	 */
+    // フィールド	
+	// userName    名前
+	// amount      金額
+	// grades      成績
 	String userName;
 	int amount;
-	String ranking;
+	String grades;
 	
 	/**
-	 * 子供たちに名前をお小遣いを授ける
-	 * @param inputName    子供の名前
+	 * 母から名前とお小遣いを授かるメソッド
+	 * @param inputName    名前
 	 * @param inputAmount  お小遣い
 	 */
 	void input(String inputName, int inputAmount) {
@@ -21,24 +21,31 @@ public class User {
 	}
 	
 	/**
-	 * 子供のお金の使い方に対して子供にランキングを発表する
+	 * 学校からの成績表を受け取るメソッド
 	 */
-	void ranking() {
-		if (amount >= 1000000) {
-			ranking = "お金の使い方が上手";
-		} else if (amount >= 10000) {
-			ranking = "お金の使い方が普通";
-		} else if (amount == 0) {
-			ranking = "お金の使い方が荒い";
-		} 
+	String gradebook() {
+        // 科目に対する評価が10段階で渡される	
+		Random rand = new Random();
+		int num = rand.nextInt(10) + 1;
+		
+        // 総合評価が A, B, C で渡される	
+		if (num >= 8) {
+			this.grades = "A";
+		} else if (num >= 5) {
+			this.grades = "B";
+		} else {
+			this.grades = "C";
+		}
+		
+		return this.grades;
 	}
 	
 	/**
-	 * 子供に対して自分の状態を教えるようにお願いする
+	 * 自分の状態を伝えるメソッド
 	 */
 	void output() {
-		System.out.println("名前:" + userName);
-		System.out.println("金額:" + amount);
-		System.out.println("ランキング:" + ranking);
+		System.out.println("名前:" + this.userName);
+		System.out.println("金額:" + this.amount);
+		System.out.println("成績:" + this.grades);
 	}
 }
